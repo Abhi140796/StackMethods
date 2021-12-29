1e099de9ac795fa734fd2c36eb3aceca2026e826
Abhishek Dubey <Abhishek_Dubey@odysseussolutions.com>
2021-12-29 09:17:13 -0500

    Add project files.

---
+++ StackMethods/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+namespace StackMethods
+{
+    /// <summary>
+    /// Stack class contains methods to add elements,remove elements and clear all elements
+    /// </summary>
+    public class Stack
+    {
+        //List of object type
+        List<object> objectList = new List<object>();
+
+        /// <summary>
+        /// Insert given input in List
+        /// </summary>
+        /// <param name="newObject">instance of object class</param>
+        /// 
+        #region Push method
+        public void Push(object newObject)
+        {
+            objectList.Add(newObject);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// removes last element from list
+        /// </summary>
+        /// <returns>value of removed element</returns>
+        #region Pop method       
+        public object Pop()
+        {
+            int sizeOfList = objectList.Count;
+            object popedObject = objectList[sizeOfList - 1];
+            if (sizeOfList < 0)
+            {
+                return "List is Empty";
+            }
+            else
+            {
+                objectList.RemoveAt(sizeOfList - 1);
+                return popedObject;
+            }
+         
+        }
+
+        #endregion
+
+        /// <summary>
+        /// clear all elements exist in list
+        /// </summary>
+        /// 
+        #region Clear method
+        public void Clear()
+        {
+            objectList.Clear();
+        }
+        #endregion
+
+
+    }
+}
